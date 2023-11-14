f524e71efc6fe2364dd1dcdecb5cd0aecad46b5c
Hildah Makatu <137060024+minorah@users.noreply.github.com>
2023-11-14 17:09:04 +0200

    Added apiKey and apiUrl.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function refreshWeather(response){
+    let temperatureElement=document.querySelector("#temperature");
+    temperatureElement.innerHTML=response.data.temperature.current;
+}
+
+function searchCity (city){
+   let apiKey="7707b06c4614a73o5b83t0498f30e5af";
+   //let apiUrl=  ' https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} ' ;
+    let apiUrl=    'https://api.shecodes.io/weather/v1/current?query=${city}&key=7707b06c4614a73o5b83t0498f30e5af&units=metric"'
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handlesearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
-   let cityElement=document.querySelector("#city")
+   let cityElement=document.querySelector("#city");
    cityElement.innerHTML=searchInput.value;
+   searchCity(searchInput.value);
 }
 
 let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit" ,handlesearchSubmit);
+searchFormElement.addEventListener("submit", handlesearchSubmit);
