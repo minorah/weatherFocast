aef7706e9dc601ed5b748f31d4bd8d81d072ede0
Hildah Makatu <137060024+minorah@users.noreply.github.com>
2023-11-16 01:06:02 +0200

    Updated date, time and icon url.

--- src/index.js
+++ src/index.js
@@ -65,6 +65,13 @@ function handlesearchSubmit(event){
    cityElement.innerHTML=searchInput.value;
    searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[date.getDay()];
+  }
+  
 
 function getForecast(city){
     let apiKey="7707b06c4614a73o5b83t0498f30e5af";
@@ -77,24 +84,26 @@ function displayForecast(response) {
     console.log(response.data);
 
 
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  
     let forecastHtml = "";
   
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day , index) {
+        if(index<5){
       forecastHtml =
         forecastHtml +
         `
         <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-              <strong>15º</strong>
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
             </div>
-            <div class="weather-forecast-temperature">9º</div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
           </div>
         </div>
       `;
+        }
     });
     let forecastElement=document.querySelector("#forecast");
     forecastElement.innerHTML=forecastHtml;
